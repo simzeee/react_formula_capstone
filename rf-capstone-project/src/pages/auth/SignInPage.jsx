707a0a2bdf45ce3656c7c57fa0ae5d6fa0e49fa4
John Sims <johnsims.dev@gmail.com>
2025-01-19 18:33:13 -0500

    finish initial setup, install TW, React Router, Font Awesome, basic sign in/sign up routes

---
+++ rf-capstone-project/src/pages/auth/SignInPage.jsx
@@ -0,0 +1,5 @@
+const SignInPage = () => {
+  return <div className="font-lato text-green-500">Sign In</div>;
+};
+
+export default SignInPage;
---
+++ rf-capstone-project/src/pages/auth/SignUpPage.jsx
@@ -0,0 +1,5 @@
+const SignUpPage = () => {
+  return <div className='font-playfair text-blue-500'>Sign Up</div>;
+};
+
+export default SignUpPage;
